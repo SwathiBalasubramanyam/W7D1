ac11bbd3cd3a19a54b158b409f0611bedfa1194c
Raymond <raymondtan676@gmail.com>
2023-08-28 16:50:46 -0700

    Complete phase 4

--- practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        new_session_token = user.reset_session_token!
+        session[:session_token] = new_session_token
+    end
+
+    def logged_in?
+        !!current_user
+    end
+
+    def logged_out?
+        !logged_in?
+    end
+
+    def require_logged_out
+        redirect_to cats_url if logged_in?
+    end
 end
