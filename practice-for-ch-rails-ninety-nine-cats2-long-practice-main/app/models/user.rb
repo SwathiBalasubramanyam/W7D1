41f7440a61884250699124112652f2248dd92ff9
Raymond <raymondtan676@gmail.com>
2023-08-28 14:35:58 -0700

    Complete user model

---
+++ practice-for-ch-rails-ninety-nine-cats2-long-practice-main/app/models/user.rb
@@ -0,0 +1,35 @@
+class User < ApplicationRecord
+    before_validation :ensure_session_token
+
+    attr_reader :password
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_obj = BCrypt::Password.new(self.password_digest)
+        password_obj.is_password?(password)
+    end
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        user && user.is_password?(password) ? user : nil
+    end
+
+    def reset_session_token!
+        user.session_token = generate_unique_session_token
+        user.save!
+        user.session_token
+    end
+
+    private
+    def generate_unique_session_token
+        SecureRandom::urlsafe_base64(16)
+    end
+
+    def ensure_session_token
+        user.session_token ||= generate_unique_session_token
+    end
+end
